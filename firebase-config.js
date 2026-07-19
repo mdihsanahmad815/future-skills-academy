b3858633cc66ffd97b38da1ef7a67a3c3150df17
MD IHSAN AHMAD <mdihsanahmad815@gmail.com>
2026-07-19 09:17:03 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Firebase Config
+
+const firebaseConfig = {
+
+apiKey: "YOUR_API_KEY",
+
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+projectId: "YOUR_PROJECT_ID",
+
+storageBucket: "YOUR_PROJECT.appspot.com",
+
+messagingSenderId: "XXXXXXXX",
+
+appId: "XXXXXXXXXXXX"
+
+};
+
+// পরে এখানে Firebase initialize করা হবে।
